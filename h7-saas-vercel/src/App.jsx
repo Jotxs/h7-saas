28aa350bb4937e6b4f2d371bfe2f8c4e6b8d443e
Jota <121276461+Jotxs@users.noreply.github.com>
2025-11-20 19:21:11 -0300

    Add files via upload

---
+++ h7-saas-vercel/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{fontFamily: 'Arial', padding: 40}}>
+      <h1>H7 Barber</h1>
+      <p>SaaS de agendamento funcionando!</p>
+    </div>
+  )
+}
